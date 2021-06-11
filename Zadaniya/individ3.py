dfe08684c8df091050508a1d27d9473266ad5afa
Your Name <Alex@gmail.com>
2021-06-11 10:11:17 +0300

    Задания

--- Zadaniya/individ3.py
+++ Zadaniya/individ3.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import math
+
 for i in range(10, 100):
-    x=i//10+i%10
-    if i==x+x*x:
+    x = i//10+i%10
+    if i == x+x*x:
         print(i)
